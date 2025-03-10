eaea5c708da3a571305d30dfbe7378e683b0de0a
Nguyen Nhat Thanh <nthanh100406@gmail.com>
2025-03-11 00:16:42 +0700

    Chương trình tìm phân số lớn nhất

--- Bài tập 02.cpp
+++ Bài tập 02.cpp
@@ -5,6 +5,8 @@ int TuSo;
 int MauSo;
 };
 
+//Chương trình gọi thuộc tính tử và mẫu của kiểu Phân Số. 
+
 int ucln(int a, int b){
 if(b==0) return 0;
 int y = abs (a);
@@ -15,6 +17,9 @@ while(b!=0){
 }
 return y;
 }
+
+//Hàm gọi ước chung lớn nhất để xử lý tử và mẫu của phân số. 
+
 void Nhap(PhanSo &a){
 cin>>a.TuSo>>a.MauSo;
 while(a.MauSo==0){
@@ -22,6 +27,9 @@ while(a.MauSo==0){
     cin>>a.TuSo>>a.MauSo;
 }
 }
+
+//Hàm nhập biến kiểu phân số. Nếu tử số sai thì nhập lại. 
+
 void RutgonPhanSo(PhanSo &x){
 if(x.MauSo<0){
  x.TuSo=-x.TuSo;
@@ -31,6 +39,9 @@ if(x.MauSo<0){
  x.TuSo/=u;
  x.MauSo/=u;
 }
+
+//Gọi hàm rút gọn để đưa về phân số tối giản để dễ so sánh. Hàm này chia tử và mẫu cho ước chung lớn nhất của chúng. 
+
 void XuatKetQua(PhanSo a,PhanSo b){
 int x = a.TuSo*b.MauSo;
 int y = b.TuSo*a.MauSo;
@@ -57,13 +68,16 @@ else{
     }
 }
 }
+
+//Hàm xuất xử lý tử và mẫu sau khi rút gọn. Sau đó tiến hành so sánh chung và xuất phân số lớn hơn ra, đồng thời định dạng đưa mẫu số về dương. 
+
 int main(){
 PhanSo a,b;
 cout<<"Nhap Phan So a va b: "<<endl;
 Nhap(a);
-Nhap(b);
+Nhap(b); //Gọi hàm nhập cho biến a, b
 RutgonPhanSo(a);
-RutgonPhanSo(b);
+RutgonPhanSo(b); //Xử lý rút gọn a,b
 cout<<"Phan So Lon Nhat: ";
-XuatKetQua(a,b);
+XuatKetQua(a,b); //Xử lý và xuất phân số lớn nhất.
 }
