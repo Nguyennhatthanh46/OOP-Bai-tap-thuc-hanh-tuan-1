d178f5ee81d979c85965b0a36b5757a1410dca34
Nguyen Nhat Thanh <nthanh100406@gmail.com>
2025-03-11 00:09:32 +0700

    Chương trình rút gọn phân số

--- Bài tập 01.cpp
+++ Bài tập 01.cpp
@@ -5,6 +5,8 @@ int TuSo;
 int MauSo;
 };
 
+//Khai báo kiểu cấu trúc với thuộc tính là tử và mẫu của phân số. 
+
 int ucln(int a, int b){
 if(b==0) return 0;
 int y = abs (a);
@@ -15,6 +17,9 @@ while(b!=0){
 }
 return y;
 }
+
+//Hàm tính ước chung lớn nhất với đầu vào tử và mẫu của phân số. Thực hiện phép chia liên tục và hoán đổi giá trị để đạt đến giá trị của a và b. 
+
 void Nhap(PhanSo &a){
 cin>>a.TuSo>>a.MauSo;
 while(a.MauSo==0){
@@ -22,6 +27,9 @@ while(a.MauSo==0){
     cin>>a.TuSo>>a.MauSo;
 }
 }
+
+//Hàm nhập phân số với đầu vào là tử và mẫu của phân số. Cách thức là triển khai nhập từng thuộc tính. Nếu như mẫu sai thì phải nhập lại.
+
 void Xuat(PhanSo &x){
 if(x.TuSo ==0) {cout<<0;}
 else{
@@ -39,10 +47,14 @@ if(x.MauSo<0){
  }
 }
 }
+
+//Hàm xử lý tử và mẫu của phân số khi thực hiện hàm nhập xong, quá trình xử lý sẽ đưa mẫu số về số dương và rút gọn bằng cách chia cho ước chung của tử và mẫu số.
+
 int main(){
 PhanSo x;
-cout<<"Nhap Phan So:"<<endl;
+cout<<"Nhap Phan So:"<<endl; 
 Nhap(x);
 cout<<"Ket Qua Rut Gon Phan So:"<<endl;
-Xuat(x);
+Xuat(x); 
+        //Khai báo biến có kiểu dữ liệu Phân Số. Sau đó gọi hàm Nhập và Xuất. 
 }
